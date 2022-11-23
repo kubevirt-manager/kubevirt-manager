d8ad6480b2af0981fb398101f2758906d1bf077e
feitnomore <feitnomore@users.noreply.github.com>
2022-11-23 09:58:12 -0300

    Basic VirtualMachineClusterInstanceType screen

--- src/app/components/cluster-instance-type-list/cluster-instance-type-list.component.ts
+++ src/app/components/cluster-instance-type-list/cluster-instance-type-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
@@ -12,6 +13,22 @@ import { KubeVirtService } from 'src/app/services/kube-virt.service';
 export class ClusterInstanceTypeListComponent implements OnInit {
 
     clusterInstanceTypeList: KubeVMClusterInstanceType [] = [];
+
+    myClusterInstanceTypeTemplate = {
+        'apiVersion': "instancetype.kubevirt.io/v1alpha1",
+        'kind': "VirtualMachineClusterInstancetype",
+        'metadata':{
+          'name': ""
+        },
+        'spec': {
+            'cpu': {
+                'guest': 0
+            },
+            'memory': {
+                'guest': ""
+            }
+        }
+    };
 
     constructor(
         private router: Router,
@@ -42,6 +59,188 @@ export class ClusterInstanceTypeListComponent implements OnInit {
         }
     }
 
+    /*
+     * Show Edit Window
+     */
+    showEdit(typeName: string): void {
+        let modalDiv = document.getElementById("modal-edit");
+        let modalTitle = document.getElementById("edit-title");
+        let modalBody = document.getElementById("edit-value");
+        if(modalTitle != null) {
+            modalTitle.replaceChildren("Edit: " + typeName);
+        }
+        if(modalBody != null) {
+            let editTypeField = document.getElementById("edit-type");
+            if(editTypeField != null) {
+                editTypeField.setAttribute("value", typeName);
+            }
+        }
+        if(modalDiv != null) {
+            modalDiv.setAttribute("class", "modal fade show");
+            modalDiv.setAttribute("aria-modal", "true");
+            modalDiv.setAttribute("role", "dialog");
+            modalDiv.setAttribute("aria-hidden", "false");
+            modalDiv.setAttribute("style","display: block;");
+        }
+    }
+
+    /*
+     * Hide Edit Window
+     */
+    hideEdit(): void {
+        let modalDiv = document.getElementById("modal-edit");
+        if(modalDiv != null) {
+            modalDiv.setAttribute("class", "modal fade");
+            modalDiv.setAttribute("aria-modal", "false");
+            modalDiv.setAttribute("role", "");
+            modalDiv.setAttribute("aria-hidden", "true");
+            modalDiv.setAttribute("style","display: none;");
+        }
+    }
+
+    /*
+     * Perform Edit
+     */
+    async applyEdit(cpuSize: string, memorySize: string): Promise<void> {
+        let typeField = document.getElementById("edit-type");
+        if(cpuSize != null && memorySize != null && typeField != null) {
+            let typeName = typeField.getAttribute("value");
+            if(typeName != null) {
+                try {
+                    const data = await lastValueFrom(this.kubeVirtService.editClusterInstanceType(typeName, cpuSize, memorySize));
+                    this.hideEdit();
+                    this.reloadComponent();
+                } catch (e) {
+                    if (e instanceof HttpErrorResponse) {
+                        alert(e.error["message"])
+                    } else {
+                        console.log(e);
+                        alert("Internal Error!");
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+     * Show Delete Window
+     */
+    showDelete(typeName: string): void {
+        let modalDiv = document.getElementById("modal-delete");
+        let modalTitle = document.getElementById("delete-title");
+        let modalBody = document.getElementById("delete-value");
+        if(modalTitle != null) {
+            modalTitle.replaceChildren("Delete!");
+        }
+        if(modalBody != null) {
+            let deleteTypeInput = document.getElementById("delete-type");
+            if(deleteTypeInput != null ) {
+                deleteTypeInput.setAttribute("value", typeName);
+                modalBody.replaceChildren("Are you sure you want to delete " + typeName + "?");
+            }
+        }
+        if(modalDiv != null) {
+            modalDiv.setAttribute("class", "modal fade show");
+            modalDiv.setAttribute("aria-modal", "true");
+            modalDiv.setAttribute("role", "dialog");
+            modalDiv.setAttribute("aria-hidden", "false");
+            modalDiv.setAttribute("style","display: block;");
+        }
+    }
+
+    /*
+     * Hide Delete Window
+     */
+    hideDelete(): void {
+        let modalDiv = document.getElementById("modal-delete");
+        if(modalDiv != null) {
+            modalDiv.setAttribute("class", "modal fade");
+            modalDiv.setAttribute("aria-modal", "false");
+            modalDiv.setAttribute("role", "");
+            modalDiv.setAttribute("aria-hidden", "true");
+            modalDiv.setAttribute("style","display: none;");
+        }
+    }
+
+    /*
+     * Perform Delete
+     */
+    async applyDelete(): Promise<void> {
+        let typeField = document.getElementById("delete-type");
+        if(typeField != null) {
+            let typeName = typeField.getAttribute("value");
+            if(typeName != null) {
+                try {
+                    const data = await lastValueFrom(await this.kubeVirtService.deleteClusterInstanceType(typeName));
+                    this.hideDelete();
+                    this.reloadComponent();
+                } catch (e) {
+                    if (e instanceof HttpErrorResponse) {
+                        alert(e.error["message"])
+                    } else {
+                        console.log(e);
+                        alert("Internal Error!");
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+     * Show New Window
+     */
+    showNew(): void {
+        let modalDiv = document.getElementById("modal-new");
+        let modalTitle = document.getElementById("new-title");
+        if(modalTitle != null) {
+            modalTitle.replaceChildren("New Cluster Instance Type");
+        }
+        if(modalDiv != null) {
+            modalDiv.setAttribute("class", "modal fade show");
+            modalDiv.setAttribute("aria-modal", "true");
+            modalDiv.setAttribute("role", "dialog");
+            modalDiv.setAttribute("aria-hidden", "false");
+            modalDiv.setAttribute("style","display: block;");
+        }
+    }
+
+    /*
+     * Hide New Window
+     */
+    hideNew(): void {
+        let modalDiv = document.getElementById("modal-new");
+        if(modalDiv != null) {
+            modalDiv.setAttribute("class", "modal fade");
+            modalDiv.setAttribute("aria-modal", "false");
+            modalDiv.setAttribute("role", "");
+            modalDiv.setAttribute("aria-hidden", "true");
+            modalDiv.setAttribute("style","display: none;");
+        }
+    }
+
+    /*
+     * Create the new type
+     */
+    async applyNew(typeName: string, typeCPU: string, typeMemory: string): Promise<void> {
+        if(typeName != null && typeCPU != null && typeMemory != null) {
+            this.myClusterInstanceTypeTemplate.metadata.name = typeName;
+            this.myClusterInstanceTypeTemplate.spec.cpu.guest = Number(typeCPU);
+            this.myClusterInstanceTypeTemplate.spec.memory.guest = typeMemory + "Gi";
+            try {
+                const data = await lastValueFrom(this.kubeVirtService.createClusterInstanceType(typeName, this.myClusterInstanceTypeTemplate));
+                this.hideNew();
+                this.reloadComponent();
+            } catch (e) {
+                if (e instanceof HttpErrorResponse) {
+                    alert(e.error["message"])
+                } else {
+                    console.log(e);
+                    alert("Internal Error!");
+                }
+            }
+        }
+    }
+    
     /*
      * Reload this component
      */
