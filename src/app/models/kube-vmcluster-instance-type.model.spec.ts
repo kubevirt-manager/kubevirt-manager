e60d531357c9e224c29597aae638efa4e5662412
feitnomore <feitnomore@users.noreply.github.com>
2022-11-23 06:53:29 -0300

    New model for VirtualMachineClusterInstanceType

---
+++ src/app/models/kube-vmcluster-instance-type.model.spec.ts
@@ -0,0 +1,7 @@
+import { KubeVMClusterInstanceType } from './kube-vmcluster-instance-type.model';
+
+describe('KubeVMClusterInstanceType', () => {
+  it('should create an instance', () => {
+    expect(new KubeVMClusterInstanceType()).toBeTruthy();
+  });
+});
