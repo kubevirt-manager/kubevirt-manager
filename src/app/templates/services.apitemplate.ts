9ad28a14d8b2bcb7c2464e05b685adccd785d111
feitnomore <feitnomore@users.noreply.github.com>
2022-11-29 11:47:57 -0300

    service template

--- src/app/templates/services.apitemplate.ts
+++ src/app/templates/services.apitemplate.ts
@@ -1,2 +1,25 @@
 export class Services {
+
+    serviceTemplate = {
+        "apiVersion":"v1",
+        "kind":"Service",
+        "metadata":{
+            "name":"",
+            "namespace":"",
+            "labels": {
+                "kubevirt.io/vmpool": ""
+            }
+        },
+        "spec":{
+            "type": "ClusterIP",
+            "ports": [{
+                "port": 80,
+                "protocol": "TCP",
+                "targetPort": 90
+            }],
+            "selector":{
+                "kubevirt.io/vmpool": ""
+            }
+        }
+    };
 }
